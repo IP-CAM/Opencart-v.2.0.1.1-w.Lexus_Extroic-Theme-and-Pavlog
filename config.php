9bb22f186c81b9022d158ee7b2f2cf891050e581
Laophatgia <hieuntse02172@gmail.com>
2016-04-03 14:57:03 +0700

    add thaonguyentex
    add thaonguyentex

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/lexus/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/lexus/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\xampp\htdocs\lexus/catalog/');
+define('DIR_SYSTEM', 'C:\xampp\htdocs\lexus/system/');
+define('DIR_LANGUAGE', 'C:\xampp\htdocs\lexus/catalog/language/');
+define('DIR_TEMPLATE', 'C:\xampp\htdocs\lexus/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\xampp\htdocs\lexus/system/config/');
+define('DIR_IMAGE', 'C:\xampp\htdocs\lexus/image/');
+define('DIR_CACHE', 'C:\xampp\htdocs\lexus/system/cache/');
+define('DIR_DOWNLOAD', 'C:\xampp\htdocs\lexus/system/download/');
+define('DIR_UPLOAD', 'C:\xampp\htdocs\lexus/system/upload/');
+define('DIR_MODIFICATION', 'C:\xampp\htdocs\lexus/system/modification/');
+define('DIR_LOGS', 'C:\xampp\htdocs\lexus/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'lexus');
+define('DB_PREFIX', 'oc_');
